a377a069863e3718d6afa2f06277d98f6b0e49d1
anuragkrishna4 <kanurag1008@gmail.com>
2025-05-31 02:03:50 +0530

    init

---
+++ Sprint2/Day4/cwQ1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class cwQ1 {
+    public static void main(String[] args) {
+        List l1 = new ArrayList();
+        l1.add("Amit");
+        l1.add(1);
+
+        l1.stream()
+
+        System.out.println("List l1: " + l1);
+    }
+}
